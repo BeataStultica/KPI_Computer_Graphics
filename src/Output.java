2a026881589b293e73e44f9fc755090f22c2cdbe
vmkul <v.kulyk-2023@kpi.ua>
2022-05-26 00:13:00 +0300

    Change output interface to accept rgb matrix

--- src/Output.java
+++ src/Output.java
@@ -1,10 +1,5 @@
 package src;
 
-import java.util.ArrayList;
-
 public interface Output {
-
-    void add_element(int x, double elem);
-    void display_render_res();
-
+    void display(int[][] matrix);
 }
