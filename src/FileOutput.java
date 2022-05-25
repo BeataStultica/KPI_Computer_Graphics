a9d9849a180caed3b84bb25f58c096c2086b4164
vmkul <v.kulyk-2023@kpi.ua>
2022-05-26 00:14:50 +0300

    Accept rgb matrix in file output

--- src/FileOutput.java
+++ src/FileOutput.java
@@ -1,85 +1,45 @@
 package src;
 
-
+import java.awt.*;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
-public class FileOutput implements Output{
-    private ArrayList<ArrayList<List<Integer>>> matrix = new ArrayList<>();
-    private final int width;
-    private final int height;
-    private final String filename;
-    public FileOutput(int width, int height, String filename) {
-        this.width = width;
-        this.height = height;
-        this.filename = filename;
-        for (int i = 0; i < this.width; i++) {
-            this.matrix.add(new ArrayList());
-        }
-    }
+public class FileOutput implements Output {
+	private final String filename;
 
+	public FileOutput(String filename) {
+		this.filename = filename;
+	}
 
-    public void add_element(int x, double elem){
-        if (x < this.width) {
-            if (elem <0){
-                //elem = 0;
-                this.matrix.get(x).add(Arrays.asList( (int) Math.round(128),
-                        (int) Math.round(0), (int) Math.round(255)));
-            }else {
-                this.matrix.get(x).add(Arrays.asList((int) Math.round(255 * elem),
-                        (int) Math.round(255 * elem), (int) Math.round(255 * elem)));
-            }
-        }
-        else{
-            System.out.println("Row index out of the image size");
-        }
-    }
-    public void display_render_res(){
-        try {
-            /*FileWriter fw = new FileWriter(this.filename);
-            fw.write("P3\n");
-            fw.write(this.width);
-            fw.write(' ');
-            fw.write(this.height);
-            fw.write("\n255\n");
-            for (int x = 0; x < this.width; x++) {
-                for (int y = 0; y < this.height; y++) {
-                    List<Integer> pixel = this.matrix.get(x).get(y);
-                    for (Integer pix: pixel){
-                        fw.write(pix);
-                        fw.write(' ');
-                    }
-                    fw.write('\n');
-                }
-            }
-            fw.close();
+	public void display(int[][] matrix) {
+		try {
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(this.filename)));
+			int width = matrix.length;
+			int height = matrix[0].length;
 
-             */
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(this.filename)));
+			writer.write("P3");
+			writer.newLine();
+			writer.write(width + " " + height);
+			writer.newLine();
+			writer.write("256");
+			writer.newLine();
 
-            writer.write("P3");
-            writer.newLine();
-            writer.write(this.width+" "+this.height);
-            writer.newLine();
-            writer.write("256");
-            writer.newLine();
-            for(int row=0;row<this.width;row++){
-                for(int column=0;column<this.height;column++){
-                    writer.write(this.matrix.get(row).get(column).get(0)+" ");
-                    writer.write(this.matrix.get(row).get(column).get(1)+" ");
-                    writer.write(this.matrix.get(row).get(column).get(2)+"");
-                    writer.newLine();
-                    //if(column < this.height - 1)writer.write(" ");
-                }
-                writer.newLine();
-            }
-            writer.flush();
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+			for (int[] ints : matrix) {
+				for (int column = 0; column < height; column++) {
+					Color color = new Color(ints[column]);
 
-    }
+					writer.write(color.getRed() + " ");
+					writer.write(color.getGreen() + " ");
+					writer.write(color.getBlue() + "");
+					writer.newLine();
+				}
+
+				writer.newLine();
+			}
+
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
