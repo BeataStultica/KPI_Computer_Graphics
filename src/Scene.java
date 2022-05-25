abd04ec34587402fe3b4ba8ba090171bcb268390
vmkul <v.kulyk-2023@kpi.ua>
2022-05-26 00:12:03 +0300

    Refactor / Add option to render with shadows

--- src/Scene.java
+++ src/Scene.java
@@ -1,21 +1,19 @@
 package src;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.awt.Color;
 
 public class Scene {
-	private final ArrayList<Object> objects = new ArrayList<Object>();
+	public static final int BACKGROUND_COLOR = Color.magenta.getRGB();
+	private final ArrayList<Object> objects = new ArrayList<>();
 	private final Camera camera;
 	private final Screen screen;
 	private final DirectedLight light;
-	private final Output output;
 
-	public Scene(Camera camera, Screen screen, DirectedLight light, Output output) {
+	public Scene(Camera camera, Screen screen, DirectedLight light) {
 		this.camera = camera;
 		this.screen = screen;
 		this.light = light;
-		this.output = output;
 	}
 
 	public void addObject(Object obj) {
@@ -24,113 +22,66 @@ public class Scene {
 
 	private double calcLighting(Normal normalAtPoint) {
 		double dotProduct = light.getDirection().dot(normalAtPoint);
-		//return 1;
-		//return dotProduct;
-		if (dotProduct < 0){
+
+		if (dotProduct < 0) {
 			return 0;
-		}else{
+		} else {
 			return dotProduct;
 		}
-		/*if (dotProduct < 0) {
-			return ' ';
-		} else if (dotProduct < 0.2) {
-			return '.';
-		} else if (dotProduct < 0.5) {
-			return '*';
-		} else if (dotProduct < 0.8) {
-			return '0';
-		} else {
-			return '#';
-		}*/
 	}
 
-	// render 1 object which contains the point closest to camera
-	/*public void render(){
-		Point origin = camera.getLocation();
-		ArrayList<Output> res = new ArrayList<>();
+	private boolean lightObstructed(Ray ray, Object self) {
 		for (Object object : objects) {
-			Class op = this.output.getClass();//ConsoleOutput(screen.getWidth(), screen.getHeight());
-			try {
-				Constructor cons = op.getConstructor();
-				int[] args = {screen.getWidth(), screen.getHeight()};
-				java.lang.Object output = cons.newInstance(args);
-				res.add((Output) output);
-			}catch (InstantiationException | NoSuchMethodException ie) {
+			if (object == self) continue;
+			Double intersection = object.intersectionWith(ray);
 
-			} catch (InvocationTargetException e) {
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
-			}
-		}
-		Double tVal = Double.MAX_VALUE;
-		int obj = -1;
-		for (int x = 0; x < screen.getWidth(); x++) {
-			for (int y = 0; y < screen.getHeight(); y++) {
-				Point dest = screen.getPoint(x, y);
-				Vector direction = dest.sub(origin);
-				Ray ray = new Ray(direction, origin);
-
-
-				for (int o=0; o<objects.size(); o++) {
-					Double ttval = objects.get(o).intersectionWith(ray);
-					if (ttval != null && ttval < tVal) {
-
-						tVal = ttval;
-						obj = o;
-					}
-
-					if (ttval !=null) {
-						Point intersectionPoint = ray.getPointAt(ttval);
-						Normal normalAtPoint = objects.get(o).getNormalAtPoint(intersectionPoint);
-						res.get(o).add_element(x, calcLighting(normalAtPoint));//.get(x).add(calcLighting(normalAtPoint));
-					} else {
-						res.get(o).add_element(x,0);//get(x).add('-');
-
-					}
-				}
+			if (intersection != null) {
+				return true;
 			}
 		}
 
-		res.get(obj).display_render_res();
+		return false;
+	}
 
-	}*/
-
-	// render where near objects overlap distant objects
-
-	public void render_mult() {
+	public void render(Output output, boolean withShadows) {
 		Point origin = camera.getLocation();
-
+		int[][] matrix = new int[screen.getWidth()][screen.getHeight()];
 
 		for (int x = 0; x < screen.getWidth(); x++) {
-			System.out.println(x);
 			for (int y = 0; y < screen.getHeight(); y++) {
 				Point dest = screen.getPoint(x, y);
 				Vector direction = dest.sub(origin);
 				Ray ray = new Ray(direction, origin);
 
-				Double tVal = Double.MAX_VALUE;
+				double tVal = Double.MAX_VALUE;
 				Object obj = null;
+
 				for (Object object : objects) {
 					Double ttval = object.intersectionWith(ray);
-					if (ttval !=null && ttval < tVal){
+
+					if (ttval != null && ttval < tVal) {
 						tVal = ttval;
 						obj = object;
 					}
 				}
+
 				if (obj != null) {
 					Point intersectionPoint = ray.getPointAt(tVal);
 					Normal normalAtPoint = obj.getNormalAtPoint(intersectionPoint);
-					this.output.add_element(x, calcLighting(normalAtPoint));
 
+					if (withShadows && lightObstructed(new Ray(light.getDirection(), intersectionPoint), obj)) {
+						matrix[x][y] = Color.BLACK.getRGB();
+					} else {
+						double lighting = calcLighting(normalAtPoint);
+						int shade = (int) Math.round(lighting * 255);
+						matrix[x][y] = new Color(shade, shade, shade).getRGB();
+					}
 				} else {
-					this.output.add_element(x, -1.0);
-
+					matrix[x][y] = BACKGROUND_COLOR;
 				}
-
 			}
-
 		}
-		this.output.display_render_res();
+
+		output.display(matrix);
 	}
 }
