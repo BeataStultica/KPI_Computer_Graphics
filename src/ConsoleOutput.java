9f23984641b1bad51b59f4a6834b975817221d1f
vmkul <v.kulyk-2023@kpi.ua>
2022-05-26 00:14:19 +0300

    Accept rgb matrix in console output

--- src/ConsoleOutput.java
+++ src/ConsoleOutput.java
@@ -1,48 +1,32 @@
 package src;
 
-import java.util.ArrayList;
+import java.awt.*;
 
-public class ConsoleOutput implements Output{
-    private ArrayList<ArrayList<Double>> matrix = new ArrayList<>();
-    private final int width;
-    private final int height;
-    public ConsoleOutput(int width, int height) {
-        this.width = width;
-        this.height = height;
-        for (int i = 0; i < this.width; i++) {
-            this.matrix.add(new ArrayList());
-        }
-    }
+public class ConsoleOutput implements Output {
+	public void display(int[][] matrix) {
+		int height = matrix[0].length;
 
+		for (int[] ints : matrix) {
+			for (int y = 0; y < height; y++) {
+				int rgb = ints[y];
+				double shade = new Color(rgb).getRed() / 255.0;
 
-    public void add_element(int x, double elem){
-        if (x < this.width) {
-            this.matrix.get(x).add(elem);
-        }
-        else{
-            System.out.println("Row index out of the image size");
-        }
-    }
-    public void display_render_res(){
-        for (int x = 0; x < this.width; x++) {
-            for (int y = 0; y < this.height; y++) {
-                double dot = this.matrix.get(x).get(y);
-                if (dot < 0) {
-                    System.out.print('-');
-                } else if (dot == 0){
-                    System.out.print(' ');
-                } else if (dot < 0.2) {
-                    System.out.print('.');
-                } else if (dot < 0.5) {
-                    System.out.print('*');
-                } else if (dot < 0.8) {
-                    System.out.print('0');
-                } else {
-                    System.out.print('#');
-                }
-                System.out.print(" ");
-            }
-            System.out.print("\n");
-        }
-    }
+				if (rgb == Scene.BACKGROUND_COLOR) {
+					System.out.print('-');
+				} else if (shade == 0) {
+					System.out.print(' ');
+				} else if (shade < 0.2) {
+					System.out.print('.');
+				} else if (shade < 0.5) {
+					System.out.print('*');
+				} else if (shade < 0.8) {
+					System.out.print('0');
+				} else {
+					System.out.print('#');
+				}
+				System.out.print(" ");
+			}
+			System.out.print("\n");
+		}
+	}
 }
